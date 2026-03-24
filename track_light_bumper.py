d8657cc24b8d6b18bb179ec56a6159fdd03710cc
Kendell Richards <KTibow@users.noreply.github.com>
2026-03-24 14:23:00 -0700

    turnen

--- track_light_bumper.py
+++ track_light_bumper.py
@@ -1,9 +1,11 @@
+import struct
 import threading
 import time
 
 from serial import Serial
 
 from lib.interface import (
+    OPCODE_DRIVE_DIRECT,
     OPCODE_LEDS,
     OPCODE_SAFE,
     OPCODE_SCHEDULE_DISPLAY_ASCII,
@@ -24,14 +26,28 @@ sensor_data: SensorBox[list[int]] = SensorBox()
 
 
 def control_thread():
+    turning = False
+    buttons_last_pressed = False
+    last_drive_bytes = b""
     last_led_bytes = b""
     last_digit_bytes = b""
     while True:
         sensor_data_fixed = sensor_data.get()
         bumper = sensor_data_fixed[0]
+        buttons_pressed = sensor_data_fixed[1]
+
+        if buttons_last_pressed and not buttons_pressed:
+            turning = not turning
+        buttons_last_pressed = buttons_pressed
+
         print(bumper)
         bumper_low_range = min(bumper * 16, 255)
         bumper_high_range = (bumper * 16) // 256
+        drive_bytes = (
+            OPCODE_DRIVE_DIRECT
+            + struct.pack(">h", -100 if turning else 0)
+            + struct.pack(">h", 100 if turning else 0)
+        )
         led_bytes = OPCODE_LEDS + bytes(
             [
                 0,
@@ -42,6 +58,9 @@ def control_thread():
         digit_bytes = OPCODE_SCHEDULE_DISPLAY_ASCII + str(bumper).encode("utf-8").rjust(
             4, b" "
         )
+        if drive_bytes != last_drive_bytes:
+            roomba.write(drive_bytes)
+            last_drive_bytes = drive_bytes
         if led_bytes != last_led_bytes:
             roomba.write(led_bytes)
             last_led_bytes = led_bytes
@@ -52,7 +71,7 @@ def control_thread():
 
 
 def main():
-    PACKETS = (48,)
+    PACKETS = (48, 18)
     roomba.write(OPCODE_STREAM_SENSORS + bytes((len(PACKETS),)) + bytes(PACKETS))
     roomba.read_all()
 
